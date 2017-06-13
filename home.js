5104588c384456eae41c5ab1ff13b4a582f2143b
xxabhi04 <anandabhinav16@gmail.com>
2017-06-13 17:54:32 +0530

    Display response text on home

--- home.js
+++ home.js
@@ -2,11 +2,25 @@ const on=document.getElementById('on');
 const blink=document.getElementById('blink');
 const brighten=document.getElementById('brighten');
 const submit_brightness=document.getElementById('submit_brightness');
+const status=document.getElementById('status');
 
 let connect=function(url){
     let xhr=new XMLHttpRequest();
     xhr.open('GET',url,true);
     xhr.send(null);
+
+    xhr.onreadystatechange=function () {
+        if(xhr.readyState===XMLHttpRequest.DONE){
+            if(xhr.status===200){
+                status.innerHTML=xhr.responseText;
+            }
+            else{
+                status.innerHTML="Some error occured";
+            }
+        }
+
+        
+    }
 
 };
 
